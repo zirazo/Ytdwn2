27bb4c1f237eb38671f31021a74fe3adee5e5c11
hicham zaitouni <hichamzaitouni@example.com>
2023-12-15 21:38:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// app.js
+const express = require('express');
+const ytdl = require('ytdl-core');
+const progress = require('progress');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/download', async (req, res) => {
+  const { url, format } = req.query;
+
+  try {
+    const info = await ytdl.getInfo(url);
+    const videoFormat = ytdl.chooseFormat(info.formats, { quality: format });
+
+    const totalSize = parseInt(videoFormat.contentLength, 10);
+    const progressBar = new progress('[:bar] :percent :etas', {
+      total: totalSize,
+      width: 40,
+      clear: true,
+    });
+
+    res.header('Content-Disposition', `attachment; filename="${info.title}.${videoFormat.container}"`);
+
+    ytdl(url, { format: videoFormat })
+      .on('progress', (chunkLength, downloaded, total) => {
+        progressBar.tick(chunkLength);
+      })
+      .on('end', () => {
+        progressBar.complete();
+      })
+      .pipe(res);
+  } catch (error) {
+    res.status(500).send('Error downloading video.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
