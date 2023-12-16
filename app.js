baf847a180f0260104f53461ab4e7431d2c057af
hicham zaitouni <hichamzaitouni@example.com>
2023-12-16 22:15:28 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // app.js
 const express = require('express');
 const ytdl = require('ytdl-core');
-const axios = require('axios'); // Add Axios for HTTP requests
 const progress = require('progress');
 const app = express();
 const PORT = 3000;
@@ -16,32 +15,18 @@ app.get('/download', async (req, res) => {
   const { url, format } = req.query;
 
   try {
-    // Get video info using ytdl
     const info = await ytdl.getInfo(url);
-
-    // Fetch video title from YouTube API
-    const videoId = info.videoDetails.videoId;
-    const apiKey = 'AIzaSyDF0UV7UA-XjlfViwiO_c3Ej0qIl51nG5o'; // Replace with your YouTube Data API key
-    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`;
-
-    const response = await axios.get(apiUrl);
-    const videoTitle = response.data.items[0].snippet.title;
-
-    // Choose video format
     const videoFormat = ytdl.chooseFormat(info.formats, { quality: format });
 
-    // Set response headers
-    res.header('Content-Disposition', `attachment; filename="${videoTitle}.${videoFormat.container}"`);
-
-    // Create progress bar
     const totalSize = parseInt(videoFormat.contentLength, 10);
     const progressBar = new progress('[:bar] :percent :etas', {
       total: totalSize,
-      width: 40,
+      width: 20,
       clear: true,
     });
 
-    // Pipe the video stream to the response
+    res.header('Content-Disposition', `attachment; filename="${info.title}.${videoFormat.container}"`);
+
     ytdl(url, { format: videoFormat })
       .on('progress', (chunkLength, downloaded, total) => {
         progressBar.tick(chunkLength);
@@ -51,7 +36,6 @@ app.get('/download', async (req, res) => {
       })
       .pipe(res);
   } catch (error) {
-    console.error(error);
     res.status(500).send('Error downloading video.');
   }
 });
