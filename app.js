fb42426592db5285bfad384755cdbb1c608b3bbb
zirazo <123901829+zirazo@users.noreply.github.com>
2023-12-29 01:05:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const progress = require('progress');
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -25,7 +26,24 @@ app.get('/download', async (req, res) => {
     });
 
     res.header('Content-Disposition', `attachment; filename="${info.title}.${videoFormat.container}"`);
+ const axios = require('axios');
 
+const options = {
+  method: 'GET',
+  url: 'https://ytstream-download-youtube-videos.p.rapidapi.com/dl',
+  params: {id: videoId},
+  headers: {
+    'X-RapidAPI-Key': '992c4a3c24msh3e40565ff4702eap152816jsn207591b3f6bb',
+    'X-RapidAPI-Host': 'ytstream-download-youtube-videos.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
     ytdl(url, { format: videoFormat })
       .on('progress', (chunkLength, downloaded, total) => {
         progressBar.tick(chunkLength);
